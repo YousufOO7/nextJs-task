6b448f42a3a92a36a30850c80bc59319a12abcef
Yousuf007 <yousufjobaer@gmail.com>
2025-01-24 01:26:52 +0600

    change for live side

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = async () => {
     const user = await getUser();
     return (
         <div className="bg-gray-100">
-            <div className="navbar container mx-auto">
-                <div className="flex-1">
+            <div className="navbar container mx-auto flex justify-between">
+                <div>
                     <a className="btn btn-ghost text-xl">Task</a>
                 </div>
                 <div>
