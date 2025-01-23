ca7c5f07df0ec9daedf691d015327f886abf7e9f
Yousuf007 <yousufjobaer@gmail.com>
2025-01-24 00:34:53 +0600

    make some changes

--- src/app/(home)/profile/page.jsx
+++ src/app/(home)/profile/page.jsx
@@ -9,11 +9,7 @@ const profile = async () => {
         <div className="container mx-auto">
             <h2 className="text-center text-3xl font-bold mt-10">Welcome to your profile</h2>
 
-           {/* {
-            user.picture && <div className="flex justify-center my-10">
-            <img src={user?.picture} className="rounded-md w-[200px] h-[200px] border border-gray-200" alt=""  />
-            </div>
-           } */}
+           
 
         </div>
     );
