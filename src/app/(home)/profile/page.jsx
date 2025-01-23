077f6b7ae7ded4e849e987b1475cf9f64a607be3
Yousuf007 <yousufjobaer@gmail.com>
2025-01-23 22:17:48 +0600

    remove all console.log

--- src/app/(home)/profile/page.jsx
+++ src/app/(home)/profile/page.jsx
@@ -5,7 +5,6 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 const profile = async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser();
-    console.log('this is user ', user)
     return (
         <div className="container mx-auto">
             <h2 className="text-center text-3xl font-bold mt-10">Welcome to {user.given_name} to your profile</h2>
